ed4a4c9bac233420caee34d54528c022419a30ee
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-03-31 00:26:54 -0400

    Added more API calls to NetworkManager

--- networkmanager.go
+++ networkmanager.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"net"
 	"strings"
+
+	"github.com/theojulienne/go-wireless/iwlib"
 )
 
 type NetworkManager struct {
@@ -21,6 +23,20 @@ func (nm *NetworkManager) ListInterfaces() ([]string, error) {
 	ret := make([]string, len(ifaces))
 	for idx, iface := range ifaces {
 		ret[idx] = iface.Name
+	}
+	return ret, nil
+}
+
+func (nm *NetworkManager) GetWifiInterfaces() ([]string, error) {
+	ret := make([]string, 0)
+	ifaces, err := nm.ListInterfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, name := range ifaces {
+		if strings.Index(name, "wlan") == 0 {
+			ret = append(ret, name)
+		}
 	}
 	return ret, nil
 }
@@ -47,4 +63,17 @@ func (nm *NetworkManager) IsWifiConnected() (bool, error) {
 		return false, errors.New("Did not find a wlan interface")
 	}
 	return false, nil
+}
+
+func (nm *NetworkManager) WifiScan(iface string) ([]string, error) {
+
+	networks, err := iwlib.GetWirelessNetworks(iface)
+	if err != nil {
+		return nil, err
+	}
+	ret := make([]string, 0)
+	for _, network := range networks {
+		ret = append(ret, network.SSID)
+	}
+	return ret, nil
 }
--- networkmanager_test.go
+++ networkmanager_test.go
@@ -22,3 +22,26 @@ func TestIsWifiConnected(t *testing.T) {
 	require.Nil(err)
 	require.True(v)
 }
+
+func TestGetWifiInterfaces(t *testing.T) {
+	require := require.New(t)
+
+	nm := New()
+	v, err := nm.GetWifiInterfaces()
+	require.Nil(err)
+	require.NotZero(len(v))
+}
+
+func TestWifiScan(t *testing.T) {
+	require := require.New(t)
+
+	// Should fail
+	nm := New()
+	v, err := nm.WifiScan("wlan8")
+	require.NotNil(err)
+	require.Zero(len(v))
+
+	v, err = nm.WifiScan("wlan0")
+	require.Nil(err)
+	require.NotZero(len(v))
+}
